f167e288547e3fb3bbde07e5e31beb9758f397c8
Justin Hager <coachjustinhager@gmail.com>
2020-11-24 17:48:24 -0800

    refactor routes and handlers

--- server.js
+++ server.js
@@ -12,20 +12,33 @@ app.use(helmet());
 
 
 /**
- * handleGetMovie 
+ * handleGetMovies
  * genre - filter by genre. case insensitive.
  * country - filter to include specified country. case insensitive.
  * vote - filter by vote >= to the given number
  */
-app.get('/', (req,res) => {
-    return res.status(200).send(`You've reached the home route. The active endpoint is '/movie'`)
-});
-
-app.get('/movie', (req, res) => {
+const handleGetMovie = (req, res) => {
     const { genre, country, avg_vote } = req.params;
 
-    return res.status(200).send(`You've reached the /Movie route. It's under construction.`)
-});
+    return res.status(200).send(`You've reached the UPDATED /Movie route. It's under construction.`)
+}
+
+/**
+ * handleGetHome
+ * friendly user message to point to where the action is
+ */
+const handleGetHome = (req, res) => {
+    return res.status(200).send(`You've reached the home route. All the action is at the '/movie' endpoint.`)
+}
+
+
+app.get('/', handleGetHome);
+
+
+app.get('/movie', handleGetMovie);
+
+
+
 
 app.listen(PORT, () => {
     console.log(`Server is running at http://localhost/${PORT}`);
